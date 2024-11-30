3c559d6b9339c3a58d6132c9d466585537b3defc
DarthMuzammil <muzammil.inamdar98@gmail.com>
2024-11-30 21:42:35 +0530

    fix

--- app/sitemap.js
+++ app/sitemap.js
@@ -19,17 +19,17 @@ export default function sitemap() {
         changeFrequency: 'weekly',
         priority: 0.5,
       },
-      {
+      {        url: 'https://krishaligroup.com',
         name: 'Landmark Tower',
         path: '/',
         coordinates: { lat: 24.4851, lng: 54.3508 }// Example coordinates for Dubai
       },
-      {
+      {        url: 'https://krishaligroup.com',
         name: 'Golden Falcon Tower',
         path: '/',
         coordinates: { lat: 24.4875, lng: 54.3592 } // Example coordinates for Abu Dhabi
       },
-      {
+      {        url: 'https://krishaligroup.com',
         name: 'Al Bustan Complex',
         path: '/',
         coordinates: { lat: 24.4298, lng: 54.4283 } // Example coordinates for Dubai
